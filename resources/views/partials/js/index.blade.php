97d51e39b72cf086a3c61cd4f51ec88c82a941ee
Backender <backender@gmail.com>
2022-10-08 04:18:34 +0300

    first

---
+++ resources/views/partials/js/index.blade.php
@@ -0,0 +1,10 @@
+<!-- plugin for charts  -->
+<script src="{{asset('assets/js/plugins/chartjs.min.js')}}" async></script>
+<!-- plugin for scrollbar  -->
+<script src="{{asset('assets/js/plugins/perfect-scrollbar.min.js')}}" async></script>
+<!-- github button -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- main script file  -->
+<script src="{{asset('assets/js/soft-ui-dashboard-tailwind.js?v=1.0.4')}}" async></script>
+{{-- <script src="../path/to/flowbite/dist/flowbite.js"></script> --}}
+{{-- <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script> --}}
